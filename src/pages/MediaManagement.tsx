76408473a852cf98b329f9592b1dbf29abb4d73b
oGsLP <1145234011@qq.com>
2026-04-13 15:18:43 +0000

    Revert "style(media): 搜索添加丝滑动画，最小加载350ms避免闪烁"
    This reverts commit 7b36d56e5d7d042780309aaa4ea1bc670a4c6856.

--- src/pages/MediaManagement.tsx
+++ src/pages/MediaManagement.tsx
@@ -240,14 +240,10 @@ export default function MediaManagement() {
                 size="sm" 
                 onClick={handleManualSearch}
                 disabled={isLoading}
-                className="h-9 px-4 transition-all duration-200"
+                className="h-9 px-4"
               >
-                {isLoading ? (
-                  <div className="w-4 h-4 border-2 border-white/30 border-t-white rounded-full animate-spin" />
-                ) : (
-                  <Search className="w-4 h-4" />
-                )}
-                <span className="ml-1">{isLoading ? '搜索中' : '搜索'}</span>
+                <Search className="w-4 h-4 mr-1" />
+                搜索
               </Button>
             </div>
           </div>
