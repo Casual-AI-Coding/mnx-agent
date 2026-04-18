5a9b4537ac91132bbfbbb9baa31c4ad89e536580
oGsLP <1145234011@qq.com>
2026-04-18 23:40:36 +0800

    feat(components): add lyrics components barrel export

---
+++ src/components/lyrics/index.ts
@@ -0,0 +1,6 @@
+// src/components/lyrics/index.ts
+
+export { LyricsTaskCard } from './LyricsTaskCard'
+export { LyricsTaskCarousel } from './LyricsTaskCarousel'
+export { LyricsHoverPreview } from './LyricsHoverPreview'
+export { LyricsPreviewModal } from './LyricsPreviewModal'
